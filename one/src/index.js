ca2a5b5cf50931906db4ffd2acfe8064429263a0
Prashantbaghel <prashant@radicalhealth.in>
2020-03-31 20:15:46 +0530

    first change

--- one/src/index.js
+++ one/src/index.js
@@ -1,6 +1,14 @@
-// import react 
-
+// import react
+import React from "react";
+import ReactDOM from "react-dom";
 //create a react component
-
-//show this component on the browser 
-
+const App = () => {
+  console.log("App loaded");
+  const name = "prashant";
+  return <div style={{ color: "red" }}> {name} abcd </div>;
+};
+//show this component on the browser
+ReactDOM.render(<App />, document.querySelector("#root"));
+/* const App = functions() {
+    return <div> Rito is my manager </div>;
+  }; */
